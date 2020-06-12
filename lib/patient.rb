ba387dbdca8a958fcd5dcdea55b7bd6487cb7fef
Megan Gomes <megan.gomes25@gmail.com>
2020-06-12 21:13:46 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,32 @@
+class Patient
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+    
+  def doctors
+    appointments.map {|appointment| appointment.doctor}
+    doctors = []
+    appointments.each do |appointment|
+      doctors << appointment.doctors
+    end
+    doctors
+  end
+  end  
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, date, doctor)
+  end  
+end
